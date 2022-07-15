c8086e788bea9a813bcbb828873dd8dab011d3f8
ShellManager <374779789@qq.com>
2022-07-15 09:58:49 +0800

    修复

--- jd/jd_ck_del.js
+++ jd/jd_ck_del.js
@@ -1,75 +1,80 @@
-const $ = new API('JD_Cookies', true);
-const delKey = '#del_condition';
-let condition = $.read(delKey);
-const CacheKey = `#CookiesJD`;
-
-let cookies = [];
+const $ = new API('JD_Cookies', true)
+const delKey = '#del_condition'
+let condition = $.read(delKey)
+const CacheKey = `#CookiesJD`
+let jdHelp = []
+let cookies = []
 try {
-  cookies = JSON.parse($.read(CacheKey) || '[]');
+  cookies = JSON.parse($.read(CacheKey) || '[]')
+  jdHelp = JSON.parse($.read('#jd_ck_remark') || '[]')
 } catch (e) {
-  console.log(e);
+  console.log(e)
 }
 try {
   if (condition) {
-    condition = condition.split(',');
-    console.log('当前删除条件');
-    console.log(JSON.stringify(condition, null, `\t`));
+    condition = condition.split(',')
+    console.log('当前删除条件')
+    console.log(JSON.stringify(condition, null, `\t`))
 
-    const delData = [];
-    const newData = cookies.filter(
-      (item, index) => {
-        const where = (
-          condition.indexOf(`${index + 1}`) === -1 ||
-          condition.indexOf(item.userName) === -1
-        );
-        if (!where) delData.push(item);
-        return where;
-      });
+    const delData = []
+    const newData = cookies.filter((item, index) => {
+      const where =
+        condition.indexOf(`${index + 1}`) !== -1 ||
+        condition.indexOf(item.userName) !== -1
+      if (where) delData.push(item.userName)
+      return !where
+    })
+    const newJDHelp = jdHelp.filter((item) => {
+      return delData.indexOf(item.username) === -1
+    })
 
     if (cookies.length !== newData.length) {
-      console.log('=============删除信息=============');
-      console.log(JSON.stringify(delData, null, `\t`));
-      console.log('=================================');
-      $.write(JSON.stringify(newData, null, `\t`), CacheKey);
+      console.log('=============删除信息=============')
+      console.log(JSON.stringify(delData, null, `\t`))
+      console.log('=================================')
+      $.write(JSON.stringify(newData, null, `\t`), CacheKey)
+      $.write(JSON.stringify(newJDHelp, null, `\t`), '#jd_ck_remark')
       $.notify(
-        '删除成功', '',
-        `已删除 CK ${cookies.length - newData.length} 条\n${delData.map(
-          item => item.userName).join('，')}`,
-      );
+        '删除成功',
+        '',
+        `已删除 CK ${cookies.length - newData.length} 条\n${delData
+          .map((item) => item.userName)
+          .join('，')}`
+      )
+      $.write('', delKey)
     }
   }
-  $.write('', delKey);
 } catch (e) {
-  $.write('', delKey);
-  console.log(e);
-  $.notify('删除失败', '', '删除条件异常，已经重置删除条件');
+  console.log(e)
+  $.notify('删除失败', '', '删除条件异常')
 }
+$.done({})
 
 function ENV() {
-  const isQX = typeof $task !== 'undefined';
-  const isLoon = typeof $loon !== 'undefined';
-  const isSurge = typeof $httpClient !== 'undefined' && !isLoon;
-  const isJSBox = typeof require == 'function' && typeof $jsbox != 'undefined';
-  const isNode = typeof require == 'function' && !isJSBox;
-  const isRequest = typeof $request !== 'undefined';
-  const isScriptable = typeof importModule !== 'undefined';
-  return {isQX, isLoon, isSurge, isNode, isJSBox, isRequest, isScriptable};
+  const isQX = typeof $task !== 'undefined'
+  const isLoon = typeof $loon !== 'undefined'
+  const isSurge = typeof $httpClient !== 'undefined' && !isLoon
+  const isJSBox = typeof require == 'function' && typeof $jsbox != 'undefined'
+  const isNode = typeof require == 'function' && !isJSBox
+  const isRequest = typeof $request !== 'undefined'
+  const isScriptable = typeof importModule !== 'undefined'
+  return { isQX, isLoon, isSurge, isNode, isJSBox, isRequest, isScriptable }
 }
 
-function HTTP(defaultOptions = {baseURL: ''}) {
-  const {isQX, isLoon, isSurge, isScriptable, isNode} = ENV();
-  const methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH'];
+function HTTP(defaultOptions = { baseURL: '' }) {
+  const { isQX, isLoon, isSurge, isScriptable, isNode } = ENV()
+  const methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH']
   const URL_REGEX =
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
 
   function send(method, options) {
-    options = typeof options === 'string' ? {url: options} : options;
-    const baseURL = defaultOptions.baseURL;
+    options = typeof options === 'string' ? { url: options } : options
+    const baseURL = defaultOptions.baseURL
     if (baseURL && !URL_REGEX.test(options.url || '')) {
-      options.url = baseURL ? baseURL + options.url : options.url;
+      options.url = baseURL ? baseURL + options.url : options.url
     }
-    options = {...defaultOptions, ...options};
-    const timeout = options.timeout;
+    options = { ...defaultOptions, ...options }
+    const timeout = options.timeout
     const events = {
       ...{
         onRequest: () => {},
@@ -77,229 +82,232 @@ function HTTP(defaultOptions = {baseURL: ''}) {
         onTimeout: () => {},
       },
       ...options.events,
-    };
+    }
 
-    events.onRequest(method, options);
+    events.onRequest(method, options)
 
-    let worker;
+    let worker
     if (isQX) {
-      worker = $task.fetch({method, ...options});
+      worker = $task.fetch({ method, ...options })
     } else if (isLoon || isSurge || isNode) {
       worker = new Promise((resolve, reject) => {
-        const request = isNode ? require('request') : $httpClient;
+        const request = isNode ? require('request') : $httpClient
         request[method.toLowerCase()](options, (err, response, body) => {
-          if (err) reject(err);
+          if (err) reject(err)
           else
             resolve({
               statusCode: response.status || response.statusCode,
               headers: response.headers,
               body,
-            });
-        });
-      });
+            })
+        })
+      })
     } else if (isScriptable) {
-      const request = new Request(options.url);
-      request.method = method;
-      request.headers = options.headers;
-      request.body = options.body;
+      const request = new Request(options.url)
+      request.method = method
+      request.headers = options.headers
+      request.body = options.body
       worker = new Promise((resolve, reject) => {
-        request.loadString().then((body) => {
-          resolve({
-            statusCode: request.response.statusCode,
-            headers: request.response.headers,
-            body,
-          });
-        }).catch((err) => reject(err));
-      });
+        request
+          .loadString()
+          .then((body) => {
+            resolve({
+              statusCode: request.response.statusCode,
+              headers: request.response.headers,
+              body,
+            })
+          })
+          .catch((err) => reject(err))
+      })
     }
 
-    let timeoutid;
+    let timeoutid
     const timer = timeout
       ? new Promise((_, reject) => {
-        timeoutid = setTimeout(() => {
-          events.onTimeout();
-          return reject(
-            `${method} URL: ${options.url} exceeds the timeout ${timeout} ms`,
-          );
-        }, timeout);
-      })
-      : null;
+          timeoutid = setTimeout(() => {
+            events.onTimeout()
+            return reject(
+              `${method} URL: ${options.url} exceeds the timeout ${timeout} ms`
+            )
+          }, timeout)
+        })
+      : null
 
     return (
       timer
         ? Promise.race([timer, worker]).then((res) => {
-          clearTimeout(timeoutid);
-          return res;
-        })
+            clearTimeout(timeoutid)
+            return res
+          })
         : worker
-    ).then((resp) => events.onResponse(resp));
+    ).then((resp) => events.onResponse(resp))
   }
 
-  const http = {};
+  const http = {}
   methods.forEach(
     (method) =>
-      (http[method.toLowerCase()] = (options) => send(method, options)),
-  );
-  return http;
+      (http[method.toLowerCase()] = (options) => send(method, options))
+  )
+  return http
 }
 
 function API(name = 'untitled', debug = false) {
-  const {isQX, isLoon, isSurge, isNode, isJSBox, isScriptable} = ENV();
+  const { isQX, isLoon, isSurge, isNode, isJSBox, isScriptable } = ENV()
   return new (class {
     constructor(name, debug) {
-      this.name = name;
-      this.debug = debug;
+      this.name = name
+      this.debug = debug
 
-      this.http = HTTP();
-      this.env = ENV();
+      this.http = HTTP()
+      this.env = ENV()
 
       this.node = (() => {
         if (isNode) {
-          const fs = require('fs');
+          const fs = require('fs')
 
           return {
             fs,
-          };
+          }
         } else {
-          return null;
+          return null
         }
-      })();
-      this.initCache();
+      })()
+      this.initCache()
 
       const delay = (t, v) =>
-        new Promise(function(resolve) {
-          setTimeout(resolve.bind(null, v), t);
-        });
+        new Promise(function (resolve) {
+          setTimeout(resolve.bind(null, v), t)
+        })
 
-      Promise.prototype.delay = function(t) {
-        return this.then(function(v) {
-          return delay(t, v);
-        });
-      };
+      Promise.prototype.delay = function (t) {
+        return this.then(function (v) {
+          return delay(t, v)
+        })
+      }
     }
 
     // persistance
 
     // initialize cache
     initCache() {
-      if (isQX) this.cache = JSON.parse($prefs.valueForKey(this.name) || '{}');
+      if (isQX) this.cache = JSON.parse($prefs.valueForKey(this.name) || '{}')
       if (isLoon || isSurge)
-        this.cache = JSON.parse($persistentStore.read(this.name) || '{}');
+        this.cache = JSON.parse($persistentStore.read(this.name) || '{}')
 
       if (isNode) {
         // create a json for root cache
-        let fpath = 'root.json';
+        let fpath = 'root.json'
         if (!this.node.fs.existsSync(fpath)) {
           this.node.fs.writeFileSync(
             fpath,
             JSON.stringify({}),
-            {flag: 'wx'},
-            (err) => console.log(err),
-          );
+            { flag: 'wx' },
+            (err) => console.log(err)
+          )
         }
-        this.root = {};
+        this.root = {}
 
         // create a json file with the given name if not exists
-        fpath = `${this.name}.json`;
+        fpath = `${this.name}.json`
         if (!this.node.fs.existsSync(fpath)) {
           this.node.fs.writeFileSync(
             fpath,
             JSON.stringify({}),
-            {flag: 'wx'},
-            (err) => console.log(err),
-          );
-          this.cache = {};
+            { flag: 'wx' },
+            (err) => console.log(err)
+          )
+          this.cache = {}
         } else {
           this.cache = JSON.parse(
-            this.node.fs.readFileSync(`${this.name}.json`),
-          );
+            this.node.fs.readFileSync(`${this.name}.json`)
+          )
         }
       }
     }
 
     // store cache
     persistCache() {
-      const data = JSON.stringify(this.cache);
-      if (isQX) $prefs.setValueForKey(data, this.name);
-      if (isLoon || isSurge) $persistentStore.write(data, this.name);
+      const data = JSON.stringify(this.cache)
+      if (isQX) $prefs.setValueForKey(data, this.name)
+      if (isLoon || isSurge) $persistentStore.write(data, this.name)
       if (isNode) {
         this.node.fs.writeFileSync(
           `${this.name}.json`,
           data,
-          {flag: 'w'},
-          (err) => console.log(err),
-        );
+          { flag: 'w' },
+          (err) => console.log(err)
+        )
         this.node.fs.writeFileSync(
           'root.json',
           JSON.stringify(this.root),
-          {flag: 'w'},
-          (err) => console.log(err),
-        );
+          { flag: 'w' },
+          (err) => console.log(err)
+        )
       }
     }
 
     write(data, key) {
-      this.log(`SET ${key}`);
+      this.log(`SET ${key}`)
       if (key.indexOf('#') !== -1) {
-        key = key.substr(1);
+        key = key.substr(1)
         if (isSurge || isLoon) {
-          return $persistentStore.write(data, key);
+          return $persistentStore.write(data, key)
         }
         if (isQX) {
-          return $prefs.setValueForKey(data, key);
+          return $prefs.setValueForKey(data, key)
         }
         if (isNode) {
-          this.root[key] = data;
+          this.root[key] = data
         }
       } else {
-        this.cache[key] = data;
+        this.cache[key] = data
       }
-      this.persistCache();
+      this.persistCache()
     }
 
     read(key) {
-      this.log(`READ ${key}`);
+      this.log(`READ ${key}`)
       if (key.indexOf('#') !== -1) {
-        key = key.substr(1);
+        key = key.substr(1)
         if (isSurge || isLoon) {
-          return $persistentStore.read(key);
+          return $persistentStore.read(key)
         }
         if (isQX) {
-          return $prefs.valueForKey(key);
+          return $prefs.valueForKey(key)
         }
         if (isNode) {
-          return this.root[key];
+          return this.root[key]
         }
       } else {
-        return this.cache[key];
+        return this.cache[key]
       }
     }
 
     delete(key) {
-      this.log(`DELETE ${key}`);
+      this.log(`DELETE ${key}`)
       if (key.indexOf('#') !== -1) {
-        key = key.substr(1);
+        key = key.substr(1)
         if (isSurge || isLoon) {
-          return $persistentStore.write(null, key);
+          return $persistentStore.write(null, key)
         }
         if (isQX) {
-          return $prefs.removeValueForKey(key);
+          return $prefs.removeValueForKey(key)
         }
         if (isNode) {
-          delete this.root[key];
+          delete this.root[key]
         }
       } else {
-        delete this.cache[key];
+        delete this.cache[key]
       }
-      this.persistCache();
+      this.persistCache()
     }
 
     // notification
     notify(title, subtitle = '', content = '', options = {}) {
-      const openURL = options['open-url'];
-      const mediaURL = options['media-url'];
+      const openURL = options['open-url']
+      const mediaURL = options['media-url']
 
-      if (isQX) $notify(title, subtitle, content, options);
+      if (isQX) $notify(title, subtitle, content, options)
       if (isSurge) {
         $notification.post(
           title,
@@ -307,63 +315,63 @@ function API(name = 'untitled', debug = false) {
           content + `${mediaURL ? '\n多媒体:' + mediaURL : ''}`,
           {
             url: openURL,
-          },
-        );
+          }
+        )
       }
       if (isLoon) {
-        let opts = {};
-        if (openURL) opts['openUrl'] = openURL;
-        if (mediaURL) opts['mediaUrl'] = mediaURL;
+        let opts = {}
+        if (openURL) opts['openUrl'] = openURL
+        if (mediaURL) opts['mediaUrl'] = mediaURL
         if (JSON.stringify(opts) == '{}') {
-          $notification.post(title, subtitle, content);
+          $notification.post(title, subtitle, content)
         } else {
-          $notification.post(title, subtitle, content, opts);
+          $notification.post(title, subtitle, content, opts)
         }
       }
       if (isNode || isScriptable) {
         const content_ =
           content +
           (openURL ? `\n点击跳转: ${openURL}` : '') +
-          (mediaURL ? `\n多媒体: ${mediaURL}` : '');
+          (mediaURL ? `\n多媒体: ${mediaURL}` : '')
         if (isJSBox) {
-          const push = require('push');
+          const push = require('push')
           push.schedule({
             title: title,
             body: (subtitle ? subtitle + '\n' : '') + content_,
-          });
+          })
         } else {
-          console.log(`${title}\n${subtitle}\n${content_}\n\n`);
+          console.log(`${title}\n${subtitle}\n${content_}\n\n`)
         }
       }
     }
 
     // other helper functions
     log(msg) {
-      if (this.debug) console.log(msg);
+      if (this.debug) console.log(msg)
     }
 
     info(msg) {
-      console.log(msg);
+      console.log(msg)
     }
 
     error(msg) {
-      console.log('ERROR: ' + msg);
+      console.log('ERROR: ' + msg)
     }
 
     wait(millisec) {
-      return new Promise((resolve) => setTimeout(resolve, millisec));
+      return new Promise((resolve) => setTimeout(resolve, millisec))
     }
 
     done(value = {}) {
       if (isQX || isLoon || isSurge) {
-        $done(value);
+        $done(value)
       } else if (isNode && !isJSBox) {
         if (typeof $context !== 'undefined') {
-          $context.headers = value.headers;
-          $context.statusCode = value.statusCode;
-          $context.body = value.body;
+          $context.headers = value.headers
+          $context.statusCode = value.statusCode
+          $context.body = value.body
         }
       }
     }
-  })(name, debug);
+  })(name, debug)
 }
