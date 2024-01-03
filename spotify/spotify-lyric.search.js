3059390fd8c0a97e88b566d84da6dba8547bdd01
2YA <374779789@qq.com>
2024-01-03 17:07:44 +0800

    fix

--- spotify/spotify-lyric.search.js
+++ spotify/spotify-lyric.search.js
@@ -2,26 +2,13 @@
 
 需要配置 boxjs 参数 BaiDu_APP_ID，BaiDu_SECURITY_KEY
 
-原作者：@app2smile （https://github.com/app2smile）
-
-Spotify非中文歌词翻译 Surge和Loon需要>=iOS15 (仓库地址: https://github.com/app2smile/rules)
-采用百度翻译接口进行翻译,需要先免费申请百度翻译api的id和密钥,然后根据不同软件进行不同配置
-
------------申请百度翻译(有标准版和高级版 建议申请高级版)api--------------
-标准版(很可能不够用):单次最长请求1000字符,免费调用量5万字符/月,QPS=1
-高级版:单次最长请求6000字符,免费调用量100万字符/月,QPS=10
-
-    注册百度翻译个人开发者: http://api.fanyi.baidu.com/register
-    注册后如果需要认证可自行选择是否实人认证(高级版需要验证)
-    开通(通用翻译)API服务: https://fanyi-api.baidu.com/choose
-    成功后即可看到自己的appid和密钥(不要泄露给任何人): http://api.fanyi.baidu.com/manage/developer
-
 ------------软件配置(在文本模式下,填入下方内容)--------------
 
 1.Surge:
 [MITM]
 hostname = %APPEND% spclient.wg.spotify.com
 [Script]
+# 修改下方argument中的appid和securityKey
 spotify歌词请求 = type=http-request,pattern=^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/dompling/Script/master/spotify/spotify-lyric.request.js
 spotify歌词翻译 = type=http-response,pattern=^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/,requires-body=1,binary-body-mode=1,max-size=0,script-path=https://raw.githubusercontent.com/dompling/Script/master/spotify/spotify-lyric.search.js
 
@@ -614,7 +601,7 @@ const baseUrl = "https://spclient.wg.spotify.com/color-lyrics/v2/track/";
   .catch((e) => {
     console.log(`脚本异常：` + JSON.stringify(e));
     $.response.status = 404;
-    $.response.body = undefined;
+    $.response.body = null;
     return isIOS ? $.done($.response) : $.done({ response: $.response });
   });
 
@@ -801,7 +788,7 @@ function getMusic163Tracks(name) {
   };
 
   params = Object.entries(params)
-    .map(([k, v]) => `${k}=${encodeURIComponent(v)}`)
+    .map(([k, v]) => `${k}=${encodeURI(v)}`)
     .join("&");
 
   const option = {
@@ -812,6 +799,7 @@ function getMusic163Tracks(name) {
       cookie: `NMTID=00OImW8FZYRLmd7XU5cuO6K8blcSucAAAGMoACXwQ`,
     },
   };
+  
   return $.http.get(option).then((response) => {
     return JSON.parse(response.body);
   });
