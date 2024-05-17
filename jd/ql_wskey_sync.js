45623650a8b64a0e92554e3aea52d29f78a8283b
MYZ <17602951546@163.com>
2024-05-17 21:31:40 +0800

    Update ql_wskey_sync.js

--- jd/ql_wskey_sync.js
+++ jd/ql_wskey_sync.js
@@ -49,12 +49,15 @@ async function getScriptUrl() {
     }
 
   }
+  console.log(delIds)
+  console.log(addData)
+
   if (addData.delIds) await $.ql.delete(delIds);
   if (addData.length) await $.ql.add(addData);
 
   const cookieText = addData.map((item) => item.remarks).join(`\n`);
   if ($.read('mute') !== 'true') {
-    return $.notify(title, '', `已同步账号\n： ${cookieText}`);
+    return $.notify(title, '', `已同步账号：\n ${cookieText}`);
   }
 })()
   .catch((e) => {
